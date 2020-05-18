8be46241d970f607bd9805cd661d6a887494fc36
Venkateshwar Reddy <venkateshwar.reddy@justmyroots.com>
2020-05-18 23:51:34 +0530

    Updates

---
+++ precache-manifest.7a786494034522bf836f21a4c25a1684.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97dbcc374ff962d40966eb2c6428e4e3",
+    "url": "/react-redux-saga/index.html"
+  },
+  {
+    "revision": "616265eff951b75166d4",
+    "url": "/react-redux-saga/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "f98a70479cfada071333",
+    "url": "/react-redux-saga/static/js/2.c2ddb925.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-saga/static/js/2.c2ddb925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "616265eff951b75166d4",
+    "url": "/react-redux-saga/static/js/main.94fdf4bf.chunk.js"
+  },
+  {
+    "revision": "7667dff6663a4ab01b1e",
+    "url": "/react-redux-saga/static/js/runtime-main.da6280af.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-redux-saga/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/redux-saga-counter-app/precache-manifest.17324dbc40e80bd925c670f546a100c8.js"
+  "/react-redux-saga/precache-manifest.7a786494034522bf836f21a4c25a1684.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redux-saga-counter-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-saga/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
